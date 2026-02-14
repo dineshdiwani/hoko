a5c2d6f5bdef6203d26e41e23fd1bd7157bc26f7
Dinesh Diwani <dineshdiwani@gmail.com>
2026-02-14 17:37:16 +0530

    add google login UI version marker

--- client/src/pages/Auth/UserLogin.jsx
+++ client/src/pages/Auth/UserLogin.jsx
@@ -557,6 +557,9 @@ export default function UserLogin({ role = "buyer" }) {
                   {loading ? "Sending OTP..." : "Send OTP"}
                 </button>
 
+                <p className="mt-3 text-xs text-gray-500">
+                  Google Login UI v3
+                </p>
                 <GoogleLoginButton
                   onSuccess={(credential) => {
                     handleGoogleLogin(credential);
