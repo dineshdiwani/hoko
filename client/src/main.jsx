0db232ff8995764ec4fa624b52f1f90b2a702b7c
Dinesh Diwani <dineshdiwani@gmail.com>
2026-02-14 18:01:38 +0530

    stabilize google login with canonical host and CORS fixes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,23 @@ import App from "./App";
 import "./index.css";
 import { showAlert } from "./utils/dialogs";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const shouldRedirectToWww =
+  typeof window !== "undefined" &&
+  window.location.hostname === "hokoapp.in";
+
+if (shouldRedirectToWww) {
+  window.location.replace(
+    `https://www.hokoapp.in${window.location.pathname}${window.location.search}${window.location.hash}`
+  );
+}
+
+if (!shouldRedirectToWww) {
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
 
 window.alert = showAlert;
 
