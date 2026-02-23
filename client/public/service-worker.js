13531e1cd99782df66fcfc71a769fdcd26534940
Dinesh Diwani <dineshdiwani@gmail.com>
2026-02-23 10:18:31 +0530

    Fix PWA manifest icon MIME mismatch for 512 icon

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "hoko-pwa-v4";
-const APP_SHELL = ["/", "/index.html", "/manifest.json", "/icon-192.png", "/icon-512.png"];
+const CACHE_NAME = "hoko-pwa-v5";
+const APP_SHELL = ["/", "/index.html", "/manifest.json", "/icon-192.png", "/icon-512.jpg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
