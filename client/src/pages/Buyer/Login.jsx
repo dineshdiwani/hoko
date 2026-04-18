63b23f690103efb1ebe3ccd65defc3178f95feea
Dinesh Diwani <dineshdiwani@gmail.com>
2026-04-18 10:17:54 +0530

    feat: add WhatsApp login support for buyer route

--- client/src/pages/Buyer/Login.jsx
+++ client/src/pages/Buyer/Login.jsx
@@ -1,5 +1,22 @@
+import { useSearchParams } from "react-router-dom";
 import UserLogin from "../Auth/UserLogin";
+import WhatsAppLogin from "../Auth/WhatsAppLogin";
 
 export default function BuyerLogin() {
+  const [searchParams] = useSearchParams();
+  
+  const mobileFromUrl = searchParams.get("mobile") || "";
+  const ref = searchParams.get("ref") || "";
+  
+  console.log("[BuyerLogin] mobileFromUrl:", mobileFromUrl, "ref:", ref);
+  
+  // If coming from WhatsApp with mobile, show WhatsApp login
+  const isWhatsAppUser = ref === "wa" && mobileFromUrl;
+  console.log("[BuyerLogin] isWhatsAppUser:", isWhatsAppUser);
+  
+  if (isWhatsAppUser) {
+    return <WhatsAppLogin />;
+  }
+  
   return <UserLogin role="buyer" />;
 }
