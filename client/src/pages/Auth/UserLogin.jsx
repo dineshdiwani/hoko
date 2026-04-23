ce4693a33d2962c1bed9e2e33032c28b4e19819e
Dinesh Diwani <dineshdiwani@gmail.com>
2026-04-23 15:45:50 +0530

    fix: save city to user profile after selection

--- client/src/pages/Auth/UserLogin.jsx
+++ client/src/pages/Auth/UserLogin.jsx
@@ -797,6 +797,10 @@ function handleGoogleLogin(credential) {
                 
                 // Save to localStorage first
                 setSession(finalSession);
+                
+                // Update user profile with selected city
+                api.post("/buyer/profile", { city }).catch(() => {});
+                
                 localStorage.setItem("buyer_dashboard_state", JSON.stringify({
                   activeTab: redirectTab || "posts",
                   city: city,
