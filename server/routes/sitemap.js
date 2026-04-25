65efc974f7c569505d799765fe052394d4c078e1
Dinesh Diwani <dineshdiwani@gmail.com>
2026-04-25 11:45:08 +0530

    fix: remove unused textUtils import from sitemap.js

--- server/routes/sitemap.js
+++ server/routes/sitemap.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const Requirement = require("../models/Requirement");
-const { normalizeText } = require("../utils/textUtils");
 
 router.get("/", async (req, res) => {
   try {
